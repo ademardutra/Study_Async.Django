8d05e41e019664a1743efa211ab74a3b5fc209f2
forestfh <ademargato44@gmail.com>
2024-01-29 01:54:05 -0300

    fist commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Python and Django web application for students of all study/knowledge levels (Study.Async)
+
+BR: Aplicação web Python e Django para estudantes de todos os níveis de estudo/conhecimento (Study.Async)
+
+#Study_Async.Django
---
+++ study_async/flashcard/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Categoria, Flashcard
+from django.contrib.messages import constants
+from django.contrib import messages
+# Create your views here.
+
+def novo_flashcard(request):
+    if not request.user.is_authenticated:
+        return redirect('/usuarios/logar')
+        
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+        dificuldades = Flashcard.DIFICULDADE_CHOICES
+        return render(request, 'novo_flashcard.html', {'categorias':categorias, 'dificuldades':dificuldades                                       
+                                                       })
+    elif request.method == "POST":
+        pergunta = request.POST.get('pergunta')
+        resposta = request.POST.get('resposta')
+        categoria = request.POST.get('categoria')
+        dificuldade = request.POST.get('dificuldade')
+        
+        if len(pergunta.strip()) == 0 or len(resposta.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha os campos de perguntas e respostas')
+            return redirect('/flashcard/novo_flashcard/')
+        
+        flashcard = Flashcard(
+            user = request.user,
+            pergunta=pergunta,
+            resposta=resposta,
+            categoria_id=categoria,
+            dificuldade=dificuldade
+        )
+        
+        flashcard.save()
+        messages.add_message(request, constants.SUCCESS, 'Flashcard cadastrado com sucesso')
+        return redirect('/flashcard/novo_flashcard/')
+    
+    #51:52 aula2...
